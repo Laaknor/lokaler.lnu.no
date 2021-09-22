185959423e5f525ec1a8258d6e106529721de010
Daniel Jackson <daniel@snilldesign.no>
2021-09-22 13:20:52 +0200

    Extending space model to fit Figma sketches
    Added title field, not optional
    Added optional fields for number of people who would fit, organization number (will be different from SpaceOwner, and not all spaces have their own, so optional)
    Added several rich text fields, all optional
    Changed the strings lat/long to decimal values, and renamed to more idosyncratic  lat/lng

--- app/models/space.rb
+++ app/models/space.rb
@@ -5,6 +5,13 @@ class Space < ApplicationRecord
 
   has_many :facilities, dependent: :restrict_with_exception
   has_many :reviews, dependent: :restrict_with_exception
+
   belongs_to :space_owner
   belongs_to :space_type
+
+  has_rich_text :how_to_book
+  has_rich_text :who_can_use
+  has_rich_text :pricing
+  has_rich_text :terms
+  has_rich_text :more_info
 end
